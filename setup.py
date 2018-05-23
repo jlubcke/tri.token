a02d70ed175c70f214b6355080380d8b780e66b1
Johan Lübcke <johan.lubcke@trioptima.com>
2018-05-23 11:17:39 +0200

    Upgrade to python 3.6

--- setup.py
+++ setup.py
@@ -88,6 +88,8 @@ setup(
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     cmdclass={'tag': Tag,
